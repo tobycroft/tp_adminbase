898e6e41bb52a7285e42d3a9a3d0dac525206254
火线兔 <silverhawk@foxmail.com>
2022-04-30 18:04:11 +0800

    TuuzPC

--- application/common.php
+++ application/common.php
@@ -1053,6 +1053,8 @@ if (!function_exists('action_log')) {
 
             // 查询行为,判断是否执行
             $action_info = model('admin/action')->where('module', $module)->getByName($action);
+            print_r($action_info);
+            exit();
             if($action_info['status'] != 1){
                 return '该行为被禁用或删除';
             }
