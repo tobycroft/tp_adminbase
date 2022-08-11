9aa257583112af82c89a0db21b047c3738e2850c
火线兔 <silverhawk@foxmail.com>
2022-08-11 12:37:01 +0800

    TuuzMac

--- extend/Aoss/Aoss.php
+++ extend/Aoss/Aoss.php
@@ -8,10 +8,10 @@ class Aoss
 
     public function send($real_path, $mime_type, $file_name)
     {
-        return self::send_file($this->send_url, $real_path, $mime_type, $file_name);
+        return self::send_file_ret($this->send_url, $real_path, $mime_type, $file_name);
     }
 
-    public static function send_file($send_url, $real_path, $mime_type, $file_name)
+    public static function send_file_url($send_url, $real_path, $mime_type, $file_name)
     {
         $postData = [
             'file' => new \CURLFile(realpath($real_path), $mime_type, $file_name)
@@ -24,10 +24,32 @@ class Aoss
         $response = curl_exec($ch);
         curl_close($ch);
         $json = json_decode($response, true);
+        if (empty($json) || !isset($json["code"])) {
+            return false;
+        }
         if ($json["code"] == "0") {
             return $json["data"];
         } else {
             return false;
         }
+    }
+
+    public static function send_file_ret($send_url, $real_path, $mime_type, $file_name)
+    {
+        $postData = [
+            'file' => new \CURLFile(realpath($real_path), $mime_type, $file_name)
+        ];
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $send_url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        $json = json_decode($response, true);
+        if (empty($json) || !isset($json["code"])) {
+            return false;
+        }
+        return $json;
     }
 }
