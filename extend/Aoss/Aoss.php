6c98accfef0522a733832ab2f184509b88e19847
火线兔 <silverhawk@foxmail.com>
2022-08-11 12:51:50 +0800

    TuuzMac

--- extend/Aoss/Aoss.php
+++ extend/Aoss/Aoss.php
@@ -12,7 +12,7 @@ class Aoss
     }
 
 
-    public static function send_file_url($send_url, $real_path, $mime_type, $file_name)
+    public static function send_file_url($send_url, $real_path, $mime_type, $file_name): AossSimpleRet
     {
         $postData = [
             'file' => new \CURLFile(realpath($real_path), $mime_type, $file_name)
@@ -24,10 +24,10 @@ class Aoss
         curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
         $response = curl_exec($ch);
         curl_close($ch);
-
+        return new AossSimpleRet($response);
     }
 
-    public static function send_file_ret($send_url, $real_path, $mime_type, $file_name)
+    public static function send_file_ret($send_url, $real_path, $mime_type, $file_name): AossCompleteRet
     {
         $postData = [
             'file' => new \CURLFile(realpath($real_path), $mime_type, $file_name)
@@ -39,11 +39,7 @@ class Aoss
         curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
         $response = curl_exec($ch);
         curl_close($ch);
-        $json = json_decode($response, true);
-        if (empty($json) || !isset($json["code"])) {
-            return false;
-        }
-        return $json;
+        return new AossCompleteRet($response);
     }
 }
 
@@ -60,6 +56,48 @@ class AossSimpleRet
         }
         if ($json["code"] == "0") {
             $this->data = $json["data"];
+        } else {
+            return $this->error = $json["data"];
+        }
+    }
+}
+
+class AossCompleteRet
+{
+    public $error = null;
+    public $data = [];
+    public $name = "";
+    public $path = "";
+    public $mime = "";
+    public $size = 0;
+    public $ext = "";
+    public $md5 = "";
+    public $src = "";
+    public $url = "";
+    public $surl = "";
+    public $duration = 0;
+    public $duration_str = "";
+    public $bitrate = 0;
+
+    public function __construct($response)
+    {
+        $json = json_decode($response, true);
+        if (empty($json) || !isset($json["code"])) {
+            return false;
+        }
+        if ($json["code"] == "0") {
+            $this->data = $json["data"];
+            $this->name = $this->data["name"];
+            $this->path = $this->data["path"];
+            $this->mime = $this->data["mime"];
+            $this->size = $this->data["size"];
+            $this->ext = $this->data["ext"];
+            $this->md5 = $this->data["md5"];
+            $this->src = $this->data["src"];
+            $this->url = $this->data["url"];
+            $this->surl = $this->data["surl"];
+            $this->duration_str = $this->data["duration_str"];
+            $this->bitrate = $this->data["bitrate"];
         } else {
             return $this->error = $json["data"];
         }
