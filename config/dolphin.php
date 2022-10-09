4f4b3664ab40389ac532123b439d0a038397195c
火线兔 <silverhawk@foxmail.com>
2022-10-09 12:01:29 +0800

    TuuzMac

--- config/dolphin.php
+++ config/dolphin.php
@@ -4,13 +4,13 @@
 return [
     // 产品信息
     'product_name' => 'ThinkPHP',
-    'product_version' => '1.5.0',
-    'build_version' => '202102051729',
+    'product_version' => '5.1.0',
+    'build_version' => '202209051159',
     'product_website' => 'http://www.thinkphp.cn',
     'product_update' => 'http://www.thinkphp.cn/checkUpdate',
     'develop_team' => 'ThinkPHP',
 
     // 公司信息
-    'company_name' => '广东卓锐软件有限公司',
-    'company_website' => 'http://www.zrthink.com',
+    'company_name' => 'Thinkphp',
+    'company_website' => 'http://www.thinkphp.cn',
 ];
